0eb7406168d269488470646d4819d36b5226959b
invisible_man <gowrisankar2396@gmail.com>
2023-08-03 20:30:50 +0200

    Fixed Shape issue to handle measurement explosion

--- src/decentralised_adaptive_coverage/scripts/models/UAV.py
+++ src/decentralised_adaptive_coverage/scripts/models/UAV.py
@@ -24,6 +24,7 @@ class Drone:
         self.lawnmower_path_tracker = []
         self.true_sensor = sense_field
         self.measurements = []
+        self.scaling_enabled = False
         
     def compute_voronoi(self, plot=False):
         voronoi_calculator = voronoi.VoronoiCalculator(self.drone_positions, 'square', self.field_size)
@@ -31,20 +32,24 @@ class Drone:
         if plot:
             voronoi_calculator.plot_voronoi()
 
-    def plan(self):
+    def plan(self, plot=False):
         planner = planpath.Planner(self)
         planner.compute_lawnmower_path()
-        planner.plot_lawnmower_path()
+        if plot:
+            planner.plot_lawnmower_path()
         self.lawnmower_path_tracker.append(self.lawnmower_path)
 
     def sense(self):
         self.measurements = np.array([self.true_sensor(point, self.grid_points, self.true_weed_distribution) for point in self.lawnmower_path])
 
     def update_voronoi(self):
-        scaling_factor = self.grid_resolution*1000
+        if self.scaling_enabled:
+            scaling_factor = self.grid_resolution*1000
+        else:
+            scaling_factor = 1
         mv = np.sum(self.measurements)*scaling_factor
-        cx = np.sum(self.lawnmower_path[:, 0] * self.measurements) / mv
-        cy = np.sum(self.lawnmower_path[:, 1] * self.measurements) / mv
+        cx = np.sum(np.squeeze(self.lawnmower_path[:, 0]) * np.squeeze(self.measurements)) / mv
+        cy = np.sum(np.squeeze(self.lawnmower_path[:, 1]) * np.squeeze(self.measurements)) / mv
         new_center = [cx+self.grid_resolution/1000, cy+self.grid_resolution/1000, self.altitude]
         self.voronoi_center_tracker.append(new_center)
         self.position = np.array(new_center)
@@ -55,10 +60,11 @@ if __name__ == "__main__":
     size = 50
     grid_resolution = 1 
     drone_count = 16
-    weed_centers = [[-size/4, -size/4], [size/4, size/4]]
+    weed_centers = [[size/4, -size/4], [size/4, size/4]]
     weed_cov = [[5, 0], [0, 5]]
 
     field = Field(size, grid_resolution, drone_count, weed_centers, weed_cov)
+    field.plot_field()
 
     drones = [Drone(pos, field, id) for id,pos in enumerate(field.drone_positions)]
 
--- src/decentralised_adaptive_coverage/scripts/run.py
+++ src/decentralised_adaptive_coverage/scripts/run.py
@@ -1,4 +1,4 @@
-from models import Environment,Drones
+from models import Environment,UAV
 from tqdm import tqdm
 
 if __name__ == "__main__":
@@ -9,26 +9,27 @@ if __name__ == "__main__":
     drone_count = 16
     weed_centers = [[-size/4, -size/4], [size/4, size/4]]
     weed_cov = [[5, 0], [0, 5]]
-    iterations = 5
+    iterations = 10
 
     field = Environment.Field(size, grid_resolution, drone_count, weed_centers, weed_cov)
     field.plot_field()
 
-    drones = [Environment.Drones(pos, field, id) for id,pos in enumerate(field.drone_positions)]
+    drones = [UAV.Drone(pos, field, id) for id,pos in enumerate(field.drone_positions)]
 
     for iteration in tqdm(range(iterations)):
         print(f"\nIteration {iteration+1}")
 
-        for i, drone in enumerate(drones[:4]):
+        for i, drone in enumerate(drones):
             drone.compute_voronoi(plot=False)
-            drone.plan()
-            print(f"Drone {i+1} Path Length: {len(drone.lawnmower_path)}")
+            drone.plan(plot=False)
+            # print(f"Drone {i+1} Path Length: {len(drone.lawnmower_path)}")
 
             drone.sense()
-            print(f"Drone {i+1} Measurements: {drone.measurements[:10]}")
+            # print(f"Drone {i+1} Measurements: {drone.measurements[:10]}")
 
             drone.update_voronoi()
-            print(f"Drone {i+1} Centers: {drone.voronoi_center_tracker}")
+            # print(f"Drone {i+1} Centers: {drone.voronoi_center_tracker}")
+
             # Update the drone positions in the field
             field.drone_positions[i] = drone.position
 
